6e114a247fde98e770e7163bc287c95683b2012f
André Roggeri Campos <a.roggeri.c@gmail.com>
2017-06-19 19:47:28 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pynubank',
+    version='0.1',
+    url='https://github.com/andreroggeri/pynubank',
+    author='André Roggeri Campos',
+    author_email='a.roggeri.c@gmail.com',
+    license='BSD',
+    packages=['pynubank']
+)
