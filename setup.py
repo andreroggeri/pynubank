9e1660516600a94f949259465c371acf7256f5ae
André Roggeri Campos <a.roggeri.c@gmail.com>
2018-03-08 00:00:35 -0300

    Bump version to 0.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pynubank',
-    version='0.7',
+    version='0.8',
     url='https://github.com/andreroggeri/pynubank',
     author='André Roggeri Campos',
     author_email='a.roggeri.c@gmail.com',
