9a1a8796c6d0d862a7af193602b1e0df6f3f6e5f
Leonardo Gregianin <leogregianin@gmail.com>
2018-04-16 17:01:51 -0400

    fix tests

--- pynubank/nubank.py
+++ pynubank/nubank.py
@@ -48,10 +48,7 @@ class Nubank:
         }
         request = requests.post(Nubank.TOKEN_URL, json=body, headers=self.headers)
         if request.status_code != 200:
-            error_message = '{"error": "Error"}'
-            if request.text != '':
-                error_message = request.text
-            raise NuException('Authentication failed. {} ({})'.format(json.loads(error_message)['error'], request.status_code))
+            raise NuException('Authentication failed. {} ({})'.format(json.loads(request.content)['error'], request.status_code))
 
         data = json.loads(request.content.decode('utf-8'))
         self.headers['Authorization'] = 'Bearer {}'.format(data['access_token'])
