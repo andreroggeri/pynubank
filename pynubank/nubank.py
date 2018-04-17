3ef3741ade7cb99d3053c76364179252b804e75b
Leonardo Gregianin <leogregianin@gmail.com>
2018-04-16 20:58:53 -0400

    When login is failed, display message and error status code. For example: Too many requests (429) or Unauthorized (401)

--- pynubank/nubank.py
+++ pynubank/nubank.py
@@ -48,7 +48,8 @@ class Nubank:
         }
         request = requests.post(Nubank.TOKEN_URL, json=body, headers=self.headers)
         if request.status_code != 200:
-            raise NuException('Authentication failed. {} ({})'.format(json.loads(request.content)['error'], request.status_code))
+            message = '{} ({})'.format(json.loads(request.content)['error'], request.status_code)
+            raise NuException('Authentication failed. {}'.format(message))
 
         data = json.loads(request.content.decode('utf-8'))
         self.headers['Authorization'] = 'Bearer {}'.format(data['access_token'])
