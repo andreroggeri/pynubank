1c477990aaa16c12e9eb3f850f6deda1f74ae754
Victor <vctr.david@gmail.com>
2019-03-04 10:03:44 -0300

    added possibility to get a new token via refresh_token

--- pynubank/nubank.py
+++ pynubank/nubank.py
@@ -19,12 +19,19 @@ class Nubank:
     feed_url = None
     proxy_list_url = None
     query_url = None
+    access_token = None
+    refresh_token = None
+    refresh_before = None
 
-    def __init__(self, cpf, password):
+    def __init__(self, cpf=None, password=None, refresh_token=None):
         self.proxy_list_url = self._get_proxy_urls()
         self.TOKEN_URL = self.proxy_list_url['login']
-
-        self.authenticate(cpf, password)
+        if cpf is not None and password is not None:
+            self.authenticate(cpf, password)
+        elif refresh_token is not None:
+            self.authenticate(cpf, password, refresh_token)
+        else:
+            raise NuException('Authentication failed. Login using NuBank website, make all steps of authentication and try again.')
 
     @staticmethod
     def _get_query(query_name):
@@ -48,14 +55,22 @@ class Nubank:
             raise NuException('Something wrong with your request. Check and try again. {}'.format(message))
         return json.loads(request.content.decode('utf-8'))
 
-    def authenticate(self, cpf, password):
-        body = {
-            "grant_type": "password",
-            "login": cpf,
-            "password": password,
-            "client_id": "other.conta",
-            "client_secret": "yQPeLzoHuJzlMMSAjC-LgNUJdUecx8XO"
-        }
+    def authenticate(self, cpf=None, password=None, refresh_token=None):
+        if cpf is not None and password is not None:
+            body = {
+                "grant_type": "password",
+                "login": cpf,
+                "password": password,
+                "client_id": "other.conta",
+                "client_secret": "yQPeLzoHuJzlMMSAjC-LgNUJdUecx8XO"
+            }
+        else:
+            body = {
+                "grant_type": "refresh_token",
+                "refresh_token": refresh_token,
+                "client_id": "other.conta",
+                "client_secret": "yQPeLzoHuJzlMMSAjC-LgNUJdUecx8XO"
+            }
         request = requests.post(self.TOKEN_URL, json=body, headers=self.headers)
         if request.status_code != 200:
             message = '{} ({})'.format(request.reason, request.status_code)
@@ -63,11 +78,22 @@ class Nubank:
 
         data = json.loads(request.content.decode('utf-8'))
         self.headers['Authorization'] = 'Bearer {}'.format(data['access_token'])
+        self.access_token = data['access_token']
+        self.refresh_token = data['refresh_token']
+        self.refresh_before = data['refresh_before']
         self.feed_url = data.get('_links', {}).get('events', {}).get('href')
         if self.feed_url is None:
             raise NuException('Authentication failed. Login using NuBank website, make all steps of authentication and try again.')
         self.query_url = data['_links']['ghostflame']['href']
         self.bills_url = data['_links']['bills_summary']['href']
+
+    def get_tokens(self):
+        tokens = {
+            'access_token': self.access_token,
+            'refresh_token': self.refresh_token,
+            'refresh_before': self.refresh_before
+        }
+        return tokens
 
     def get_card_feed(self):
         request = requests.get(self.feed_url, headers=self.headers)
