0e179f369cf53568293e38568dd8c3197412d255
André Roggeri Campos <a.roggeri.c@gmail.com>
2019-04-14 14:09:24 -0300

    chore: Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pynubank',
-    version='0.19',
+    version='1.0.0',
     url='https://github.com/andreroggeri/pynubank',
     author='André Roggeri Campos',
     author_email='a.roggeri.c@gmail.com',
