7999f782ecffa50cead2b3430086b6565bccdc73
André Roggeri Campos <a.roggeri.c@gmail.com>
2020-06-09 19:51:41 -0300

    tests: Move fixtures to a package

---
+++ tests/fixtures/__init__.py
@@ -0,0 +1,7 @@
+from .account_balance import account_balance_return
+from .account_statements import account_statements_return
+from .authentication import authentication_return
+from .bill_details import bill_details_return
+from .bills import bills_return
+from .events import events_return
+from .proxy import proxy_return
