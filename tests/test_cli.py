1b6ffa6ff46286121b3b7bed76c03c87281a1a20
André Roggeri Campos <a.roggeri.c@gmail.com>
2020-06-10 22:06:03 -0300

    test: Mock HttpClient to avoid http calls

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,4 @@
 import os
-from io import StringIO
 from pathlib import Path
 from unittest.mock import patch
 
@@ -8,6 +7,7 @@ import OpenSSL
 import pynubank
 from pynubank import NuException
 from pynubank.utils.certificate_generator import CertificateGenerator
+from pynubank.utils.http import HttpClient
 
 
 def mock_error(_):
@@ -35,8 +35,9 @@ def clean():
 @patch.object(pynubank.utils.certificate_generator.CertificateGenerator, 'request_code', mock_error)
 @patch('builtins.input', lambda *args: '12341234')
 @patch('getpass.getpass', lambda *args: '12341234')
-def test_request_code_exception_should_stop_execution():
+def test_request_code_exception_should_stop_execution(monkeypatch, proxy_return):
     from pynubank import cli
+    monkeypatch.setattr(HttpClient, 'get', lambda *args: proxy_return)
 
     clean()
     cli.main()
@@ -46,14 +47,16 @@ def test_request_code_exception_should_stop_execution():
     cert = workdir.joinpath('cert.p12')
 
     assert cert.exists() is False
+    clean()
 
 
 @patch.object(pynubank.utils.certificate_generator.CertificateGenerator, 'exchange_certs', mock_certs)
 @patch.object(pynubank.utils.certificate_generator.CertificateGenerator, 'request_code', lambda x: 'email@tld')
 @patch('builtins.input', lambda *args: '12341234')
 @patch('getpass.getpass', lambda *args: '12341234')
-def test_should_generate_certs():
+def test_should_generate_certs(monkeypatch, proxy_return):
     from pynubank import cli
+    monkeypatch.setattr(HttpClient, 'get', lambda *args: proxy_return)
 
     clean()
     cli.main()
@@ -63,3 +66,4 @@ def test_should_generate_certs():
     cert = workdir.joinpath('cert.p12')
 
     assert cert.exists() is True
+    clean()
