3259cbbd6a8e45e97a42424fce657d47fe9e2c77
André Roggeri Campos <a.roggeri.c@gmail.com>
2020-06-10 22:06:03 -0300

    test: Mock HttpClient to avoid http calls

--- tests/fixtures/proxy.py
+++ tests/fixtures/proxy.py
@@ -6,5 +6,6 @@ def proxy_return():
     return {
         'token': 'https://some-url/token',
         'login': 'https://some-url/login',
-        'lift': 'https://some-url/api/proxy/B'
+        'lift': 'https://some-url/api/proxy/B',
+        'gen_certificate': 'https://some-url/gen_cert'
     }
