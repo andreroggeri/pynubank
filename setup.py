70dfd60623d22419f0ae3b9fdd48380389ff2611
André Roggeri Campos <a.roggeri.c@gmail.com>
2020-06-16 10:36:14 -0300

    chore: Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pynubank',
-    version='2.0.0',
+    version='2.1.0',
     url='https://github.com/andreroggeri/pynubank',
     author='André Roggeri Campos',
     author_email='a.roggeri.c@gmail.com',
