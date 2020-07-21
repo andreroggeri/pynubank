137020ec0939183fd8dde5a0b8c67e4b017fcb4c
André Roggeri Campos <a.roggeri.c@gmail.com>
2020-07-20 21:40:10 -0300

    feat: Add create_money_request method
    Closes #133

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -4,6 +4,7 @@ from .authentication import authentication_return
 from .bills import bills_return
 from .bills_details import bill_details_return
 from .create_boleto import create_boleto_return
+from .create_money_request import create_money_request_return
 from .events import events_return
 from .gen_certificate import gen_certificate_return
 from .proxy import proxy_return
