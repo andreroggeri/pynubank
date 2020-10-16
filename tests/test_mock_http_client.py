747db6b0bc3afba0181264c17461801a7ef92910
Thiago Brito <contato@thiagobrito.com.br>
2020-10-16 09:19:58 -0300

    - Adapting Unit Tests in order to check get_bills different in test_mock_http_client.py;

--- tests/test_mock_http_client.py
+++ tests/test_mock_http_client.py
@@ -26,12 +26,23 @@ def test_post_invalid_url_should_throw_exception():
 
 
 def test_check_not_tested_new_methods(nubank_client):
+    default_params = {
+        'get_bill_details': {
+            'bill': {'_links': {'self': {'href': 'https://mocked-proxy-url/api/bills/abcde-fghi-jklmn-opqrst-uvxz'}}}
+        }
+    }
+
     methods = dir(nubank_client)
     for method_name in methods:
         method = getattr(nubank_client, method_name)
         if method_name[0] != '_' and callable(method):
             args = list(range(method.__code__.co_argcount - 1))
-            params = inspect.signature(method).parameters
+
+            params = default_params.get(method_name)
+            if params is None:
+                params = inspect.signature(method).parameters
+
             for index, name in enumerate(params):
-                args[index] = params[name].annotation()
+                args[index] = params[name].annotation() if type(params[name]) == inspect.Parameter else params[name]
+
             method(*args)
