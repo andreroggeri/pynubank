7ad1b29ed28cfb28d0f000348b777415520f304a
André Roggeri Campos <a.roggeri.c@gmail.com>
2021-08-30 23:42:14 -0300

    chore: Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name='pynubank',
-    version='2.13.1',
+    version='2.14.0',
     url='https://github.com/andreroggeri/pynubank',
     author='André Roggeri Campos',
     author_email='a.roggeri.c@gmail.com',
